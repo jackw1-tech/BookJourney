23d63f1c486210f9d8a701e2f0f42f97c9138e5e
jackw1-tech <107569512+jackw1-tech@users.noreply.github.com>
2024-12-09 11:59:41 +0100

    Clear cache and update .gitignore

--- lib/api.dart
+++
@@ -1,6 +0,0 @@
-class Config {
-  static const String apiBaseUrl = 'http://macbook-air-di-giacomo-3.local:8000';
-  static const String loginUrl = 'http://macbook-air-di-giacomo-3.local:8000/auth/token/login/';
-  static const String libroUrl = 'http://macbook-air-di-giacomo-3.local:8000/api/categoria-libro/';
-
-}
